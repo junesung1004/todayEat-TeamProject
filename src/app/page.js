5ba97e470caf0436b1bffb6583e284ea7544ef3d
junesung1004 <junecoding1004@gmail.com>
2024-09-07 10:16:41 +0900

    초기설정

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ export default function App() {
   return (
     <motion.div
       className={styles.container}
-      initial={{ opacity: 0 }} // 초기 상태..
+      initial={{ opacity: 0 }} // 초기 상태...
       animate={{ opacity: 1 }} // 애니메이션 상태
       exit={{ opacity: 0 }} // 페이지 나갈 때 애니메이션
       transition={{ duration: 1.5 }} // 애니메이션 지속 시간
