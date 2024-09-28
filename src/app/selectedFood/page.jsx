b8a8302aa2642dcf9e985f2f271fc7a58b09178f
junesung1004 <junecoding1004@gmail.com>
2024-09-28 15:40:23 +0900

    feat: 새로고침 && 다른페이지 갔다가 돌아와도 좋아하는 음식 체크된상태로 불러오기 성공

--- src/app/selectedFood/page.jsx
+++ src/app/selectedFood/page.jsx
@@ -48,7 +48,9 @@ export default function Page() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.imgWrap}>{/* <Image src={foodImage} alt={foodData.title} priority width={359} height={205} /> */}</div>
+      <div className={styles.imgWrap}>
+        <Image src={foodImage} alt={foodData.title} priority width={359} height={205} />
+      </div>
 
       <div className={styles.foodDescContainer}>
         <h3>{foodData.title}</h3>
